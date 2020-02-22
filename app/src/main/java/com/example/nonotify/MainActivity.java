6af72bc2a3ecdafbcde5fe45fd5fecfb8681c322
Waleed Ashfaq <wqashfaq@uwaterloo.ca>
2020-02-21 19:08:42 -0500

    Added Dialog box to ask user for permission to access dnd

--- app/src/main/java/com/example/nonotify/MainActivity.java
+++ app/src/main/java/com/example/nonotify/MainActivity.java
@@ -23,6 +23,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        dnd.checkDndPermission();
     }
 
     /*@Override
--- app/src/main/java/com/example/nonotify/dndHandler.java
+++ app/src/main/java/com/example/nonotify/dndHandler.java
@@ -2,13 +2,13 @@ package com.example.nonotify;
 
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 
-
-
-
+import static java.sql.Types.NULL;
 
 
 public class dndHandler {
@@ -16,13 +16,12 @@ public class dndHandler {
     public boolean dndPermission = false;
     private Context mainContext;
     private Activity mainActivity;
-    private NotificationManager nManager;
+
 
     //Constructor
     public dndHandler(Context context, Activity activity){
         mainContext = context;
         mainActivity = activity;
-        nManager = (NotificationManager) mainActivity.getSystemService(Context.NOTIFICATION_SERVICE);
     }
 
 
@@ -30,30 +29,62 @@ public class dndHandler {
     //Checks if app has access to Do not disturb
     //If no access then prompts user to give permission
     public void checkDndPermission(){
-
-        //TO-DO : Add Dialog Box to prompt user to to give access
-
+        NotificationManager nManager = (NotificationManager) mainActivity.getSystemService(Context.NOTIFICATION_SERVICE);
 
         if (!nManager.isNotificationPolicyAccessGranted()) {
-            Intent intent = new Intent(android.provider.Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
-            mainActivity.startActivity(intent);
-            mainActivity.overridePendingTransition(android.R.anim.slide_out_right, android.R.anim.fade_out);
+
+            dndPermission = false;
+            //Ask for permission
+            dndPermissionDialog();
         }
-        else{
+         else {
             dndPermission = true;
         }
 
-
     }
+
+
+
+    public void dndPermissionDialog(){
+
+        //Dialog box to prompt user for permission
+        AlertDialog.Builder builder = new AlertDialog.Builder(mainActivity);
+        builder.setMessage(R.string.dnd_dialog_message).setTitle(R.string.dnd_dialog_title);
+
+        //If user agrees to grant access then move to notification settings
+        builder.setPositiveButton(R.string.dnd_dialog_allow, new DialogInterface.OnClickListener(){
+            public void onClick(DialogInterface dialog, int id){
+                Intent intent = new Intent(android.provider.Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
+                mainActivity.startActivity(intent);
+                mainActivity.overridePendingTransition(NULL, NULL);
+            }
+        });
+
+        //If user doesn't allow access
+        builder.setNegativeButton(R.string.dnd_dialog_cancel, new DialogInterface.OnClickListener(){
+            public void onClick(DialogInterface dialog, int id){
+
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+
+
+
 
     //Will turn ON Do Not Disturb
     public void turnOnDnd(){
+        NotificationManager nManager = (NotificationManager) mainActivity.getSystemService(Context.NOTIFICATION_SERVICE);
         nManager.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_NONE);
     }
 
 
     //Will turn OFF Do Not Disturb
     public void turnOffDnd(){
+        NotificationManager nManager = (NotificationManager) mainActivity.getSystemService(Context.NOTIFICATION_SERVICE);
         nManager.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_ALL);
     }
 
